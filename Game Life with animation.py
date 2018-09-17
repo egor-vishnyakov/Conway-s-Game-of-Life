83747e5c7ce55f9d30ddc0e868b4f96c9e3f8732
OrangeMan <lenvampir@yandex.ru>
2018-09-17 21:15:40 +0300

    Test not my realization with animation

--- Game Life with animation.py
+++ Game Life with animation.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.cm as cm
 import matplotlib.animation as manimation
 from matplotlib.animation import ImageMagickFileWriter
 
@@ -35,19 +36,33 @@ X[23, 22:24] = 1
 X[24, 21:23] = 1
 X[25, 22] = 1
 
-# FFMpegWriter = manimation.writers['avconv']
-metadata = dict(title='Game of life', artist='JustGlowing')
-writer = ImageMagickFileWriter(fps=10, metadata=metadata)
+ax = plt.subplot(111)
+canvas = ax.figure.canvas
 
-fig = plt.figure()
-fig.patch.set_facecolor('black')
-with writer.saving(fig, "game_of_life.mp4", 200):
-    plt.spy(X)
-    plt.axis('off')
-    writer.grab_frame()
-    plt.clf()
-    for x in life(X, 50):
-        plt.spy(x)
-        plt.axis('off')
-        writer.grab_frame()
-        plt.clf()
+def run(lg2):
+        for x in life(lg2, 170):
+            plt.axis('off')
+            plt.imshow(x, cmap=plt.cm.viridis) #, interpolation='quadric')
+            canvas.draw()
+            plt.clf()
+
+manager = plt.get_current_fig_manager()
+manager.window.after(100, run, X)
+plt.show()
+
+# FFMpegWriter = manimation.writers['avconv']
+# metadata = dict(title='Game of life', artist='JustGlowing')
+# writer = ImageMagickFileWriter(fps=10, metadata=metadata)
+#
+# fig = plt.figure()
+# fig.patch.set_facecolor('black')
+# with writer.saving(fig, "game_of_life.mp4", 200):
+#     plt.spy(X)
+#     plt.axis('off')
+#     writer.grab_frame()
+#     plt.clf()
+#     for x in life(X, 50):
+#         plt.spy(x)
+#         plt.axis('off')
+#         writer.grab_frame()
+#         plt.clf()
