f637a2860180b05ea46d35b28c11b345a5f072cf
OrangeMan <lenvampir@yandex.ru>
2018-09-17 21:00:27 +0300

    Remove dead cell counting

--- LifeGame.py
+++ LifeGame.py
@@ -3,7 +3,7 @@ from CicledList import CicledList
 class LifeGame:
     """Conway's Game of Life on torus surface"""
 
-    def __init__(self): #, height):
+    def __init__(self):
         self.matrix = CicledList()
         self.next_matrix = CicledList()
 
@@ -24,47 +24,37 @@ class LifeGame:
         return out
 
     def count_cell(self, i, j):
-        """Count live and dead cells around current cell
-        Return dict('live': X, 'dead': Y)"""
+        """Count live cells around current cell"""
 
-        result = {'live': 0, 'dead': 0}
+        live = 0
         for ik in range(3):
-            # print(self.matrix[i - 1 + ik])
             for jk in range(3):
                 ii = i - 1 + ik
                 jj = j - 1 + jk
                 if ii == i and jj == j:
                     continue
 
-                # print('ii: ', ii, 'jj: ', jj)
                 if self.matrix[ii][jj]:
-                    result['live'] += 1
-                else:
-                    result['dead'] += 1
-            # print('------')
-        # print('---------')
-        return result
+                    live += 1
+
+        return live
 
     def process_cell(self, i, j):
         """Count cells around current cell and change its status"""
 
         self.next_matrix[i][j] = self.matrix[i][j]
 
-        state = self.count_cell(i, j)
-        if not self.matrix[i][j] and state['live'] == 3:
+        live = self.count_cell(i, j)
+        if not self.matrix[i][j] and live == 3:
             self.next_matrix[i][j] = True
-        elif self.matrix[i][j] and (state['live'] > 3 or state['live'] < 2):
+        elif self.matrix[i][j] and (live > 3 or live < 2):
             self.next_matrix[i][j] = False
 
     def next_step(self):
         """Form next step"""
 
-        # print(self.print())
-        # print(type(self.matrix))
-
         for i in range(len(self.matrix)):
             for j in range(len(self.matrix[0])):
-                # print(i, j)
                 self.process_cell(i, j)
 
         self.matrix = self.next_matrix.copy()
